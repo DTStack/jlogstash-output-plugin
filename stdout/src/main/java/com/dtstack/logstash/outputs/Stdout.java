ab4ebccf6c7713536a9fa4dbf4047745a8d32172
whiletrue <670694243@qq.com>
2016-09-23 17:27:40 +0800

    Update Stdout.java
    增加输出内容样式

--- stdout/src/main/java/com/dtstack/logstash/outputs/Stdout.java
+++ stdout/src/main/java/com/dtstack/logstash/outputs/Stdout.java
@@ -1,6 +1,8 @@
 package com.dtstack.logstash.outputs;
 
 import java.util.Map;
+import java.util.Set;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,11 +13,17 @@ import org.slf4j.LoggerFactory;
  * Date: 2016年8月31日 下午1:36:42
  * Company: www.dtstack.com
  * @author sishu.yss
+ * @param <E>
  *
  */
-public class Stdout extends BaseOutput {
-	
+public class Stdout extends BaseOutput {	
 	private static final Logger logger = LoggerFactory.getLogger(Stdout.class);
+	
+	private static String codec="line";
+	
+	private static ObjectMapper objectMapper = new ObjectMapper();
+	
+	private static String tempalte="\"%s\" => \"%s\","+System.getProperty("line.separator");
 
 	public Stdout(Map config) {
 		super(config);
@@ -28,6 +36,23 @@ public class Stdout extends BaseOutput {
 
 	@Override
 	protected void emit(Map event) {
-		System.out.println(event);
+		try{
+			if (codec.equals("line")){
+				System.out.println(event.get("message"));
+			}else if(codec.equals("json_lines")){
+				System.out.println(objectMapper.writeValueAsString(event));
+			}else if(codec.equals("java_lines")){
+				StringBuilder sb = new StringBuilder();
+				sb.append("{").append(System.getProperty("line.separator"));
+				Set<Map.Entry<String, Object>> entrys=event.entrySet();
+				for(Map.Entry<String, Object> entry:entrys){
+					sb.append(String.format(tempalte, entry.getKey(),entry.getValue()));
+				}
+				sb.append("}");
+				System.out.println(sb.toString());
+			}
+		}catch(Exception e){
+			logger.error("Stdout emit erro",e.getCause());
+		}
 	}
 }

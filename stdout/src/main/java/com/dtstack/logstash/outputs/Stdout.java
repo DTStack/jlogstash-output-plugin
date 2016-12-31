1b9d0e2baad37e091c37658757d7d276df7efdb0
sishu@dtstack.com <sishu@dtstack.com>
2016-12-31 10:13:29 +0800

    add license

--- stdout/src/main/java/com/dtstack/logstash/outputs/Stdout.java
+++ stdout/src/main/java/com/dtstack/logstash/outputs/Stdout.java
@@ -1,7 +1,25 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.dtstack.logstash.outputs;
 
 import java.util.Map;
 import java.util.Set;
+
 import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +34,7 @@ import org.slf4j.LoggerFactory;
  * @param <E>
  *
  */
+@SuppressWarnings("serial")
 public class Stdout extends BaseOutput {	
 	private static final Logger logger = LoggerFactory.getLogger(Stdout.class);
 	
